de2c4cd086f705e8680988a4e7365ecae6e67df8
rafazingano <rafazingano@gmail.com>
2020-01-21 12:24:40 -0300

    Update all

---
+++ src/Controllers/TaskStepController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace ConfrariaWeb\Task\Controllers;
+
+use ConfrariaWeb\Task\Requests\StoreTaskTypeRequest;
+use ConfrariaWeb\Task\Requests\UpdateTaskTypeRequest;
+use App\Http\Controllers\Controller;
+
+class TaskStepController extends Controller
+{
+
+    protected $data;
+
+    public function __construct()
+    {
+        $this->data = [];
+    }
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(StoreTaskTypeRequest $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(UpdateTaskTypeRequest $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function trashed()
+    {
+        //
+    }
+}
